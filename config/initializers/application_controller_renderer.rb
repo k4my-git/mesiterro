a2897989092fb28e091b4e28b3c26f32805c69c0
k4my-git <kambara0229@icloud.com>
2020-07-02 00:15:12 +0900

    web_app_2

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '2964af70f25acfc71f6022d8e576016a9db7c26cbc450cf6da9d11ae279db2dbfdbe6128f2c9ea3415680af79948de4956fbb6948cd4dd7ea5c6e2546067b6e5'
